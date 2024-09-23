bc6370ebb582be4c4bfa9b388bcd3bc567f00e40
johaneris <jsavalos@uamv.edu.ni>
2024-09-23 08:26:10 -0600

    cuarto commit, clase persona

---
+++ CorteI/CorteI/models/Operaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorteI.models
+{
+    internal class Operaciones
+    {
+        // metodo
+        public int CalcularEdad(Persona persona)
+        {
+            int edad = DateTime.Now.Year - persona.FechaNacimiento.Year;
+            if (DateTime.Now.DayOfYear < persona.FechaNacimiento.DayOfYear)
+            {
+                edad--; // Si no ha cumplido años, resta 1
+            }
+            return edad;
+        }
+
+        // metodo
+        public string EsMayorDeEdad(Persona persona)
+        {
+            int edad = CalcularEdad(persona);
+            if (edad >= 18)
+            {
+                return "Mayor de edad";
+            }
+            else
+            {
+                return "Menor de edad";
+            }
+        }
+    }
+}
